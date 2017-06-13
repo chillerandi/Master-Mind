90b67b606d51248d24271e59d1609884da35a354
Andreas Schiller <schiller_andi@hotmail.com>
2017-06-13 12:09:33 +0200

    Game.cs : difficulty = GetGameDifficulty() implementiert (1-3); allowedAttempts spiegelt difficulty wieder: allowedAttempts = difficulty * numberCount; Ausgabe der übrigen Versuche nach letztem Spielzug           GetPlayerName() implementiert;

--- MasterMind_Solution/MasterMind/Models/Game.cs
+++ MasterMind_Solution/MasterMind/Models/Game.cs
@@ -10,15 +10,45 @@ namespace MasterMind
     {
         public Game()
         {
-            Console.WriteLine("************** Let's play Master-Mind **************\n");
+            Console.WriteLine("************** Lass uns Master-Mind spielen!! **************\n");
             string name = GetPlayerName();
-
             do {
                 Play();
 
-                Console.Write("\nWould you like to play again (Y/N)? ");
+                Console.Write("\nWürdest Du gerne nochmal spielen (J/N)? ");
             }
-            while (Console.ReadLine().ToUpper() == "Y");
+            while (Console.ReadLine().ToUpper() == "J");
+        }
+
+       
+
+        private static void Play()
+        {
+            int numberCount = GetRandomNumberCount();
+            int[] PCArray = GenerateRandomNumber(numberCount);
+            Console.Write(numberCount + " sind es! Lass uns spielen.");
+            Console.WriteLine();
+
+            int difficulty = GetGameDifficulty();
+
+            bool won = false;
+
+            for (int allowedAttempts = difficulty * numberCount; allowedAttempts > 0 && !won; allowedAttempts--) {
+                Console.WriteLine("\nGib Deinen Tip ein ({0} Versuche übrig)", allowedAttempts);
+
+                int[] userArray = GetUserGuess(numberCount);
+                if (CountHits(PCArray, userArray) == numberCount)
+                    won = true;
+            }
+
+            if (won)
+                Console.WriteLine("Du hast gewonnen!!");
+            else
+                Console.WriteLine("Oh nein! Du hast die Zahl nicht erraten.");
+            Console.Write("Die richtige Zahl lautet: ");
+            for (int j = 0; j < numberCount; j++)
+                Console.Write(PCArray[j] + " ");
+            Console.WriteLine();
         }
 
         private static string GetPlayerName()
@@ -29,35 +59,22 @@ namespace MasterMind
             return name;
         }
 
-        private static void Play()
+        public static int GetGameDifficulty()
         {
-            int numberCount = GetRandomNumberCount();
-            int[] PCArray = GenerateRandomNumber(numberCount);
-            bool won = false;
-            while (won == false) {
-                int[] userGuess = GetUserGuess(numberCount);
-                if (CountHits(PCArray, userGuess) == numberCount)
-                    won = true;
-            }
-            if (won)
-                Console.WriteLine("You win!");
-            else
-                Console.WriteLine("Oh nein! Du hast die Zahl nicht erraten.");
-            Console.Write("Die richtige Zahl lautet: ");
-            for (int j = 0; j < numberCount; j++)
-                Console.Write(PCArray[j] + " ");
-            Console.WriteLine();
+            int difficulty = 0;
+            Console.Write("Wähle einen Schwierigkeitsgrad (1=hart, 2=mittel, 3=leicht): ");
+            while (!int.TryParse(Console.ReadLine(), out difficulty) || difficulty < 1 || difficulty > 3)
+                Console.WriteLine("Ungültige Eingabe: Bitte wiederholen.");
+            return difficulty;
         }
 
         private static int CountHits(int[] PCArray, int[] userGuess)
         {
             int metaHits = 0;
-
             for (int i = 0; i < PCArray.Length; i++) {
                 if (userGuess.Any(item => item == PCArray[i]) && (PCArray[i] != userGuess[i])) {
                     metaHits++;
                 }
-
             }
             Console.WriteLine("Sie haben " + metaHits + " Fast-Treffer, die richtige Zahl, aber an der falschen Position");
 
@@ -75,7 +92,7 @@ namespace MasterMind
             int number = 0;
             int[] userGuess = new int[userSize];
             for (int i = 0; i < userSize; i++) {
-                Console.Write("Digit {0}: ", (i + 1));
+                Console.Write("Zahl {0}: ", (i + 1));
                 while (!int.TryParse(Console.ReadLine(), out number) /*|| number < 1 || number > 4*/)
                     Console.WriteLine("Ungültige Zahl!");
                 userGuess[i] = number;
