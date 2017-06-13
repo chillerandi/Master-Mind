e9248c9bb635281f6aca57ee777dc9944dc32f75
Andreas Schiller <schiller_andi@hotmail.com>
2017-06-13 11:43:32 +0200

    Game.cs : do-while-schleife(nochmal spielen? Y-N) um Play()-Methode; GetPlayerName() implementiert; meta-hits in CountHits() eingefügt--> richtige Zahlen, aber an falscher Stelle, daneben = PCArray.Length - metaHits - hits;

--- MasterMind_Solution/MasterMind/Models/Game.cs
+++ MasterMind_Solution/MasterMind/Models/Game.cs
@@ -10,8 +10,23 @@ namespace MasterMind
     {
         public Game()
         {
-            Play();
+            Console.WriteLine("************** Let's play Master-Mind **************\n");
+            string name = GetPlayerName();
 
+            do {
+                Play();
+
+                Console.Write("\nWould you like to play again (Y/N)? ");
+            }
+            while (Console.ReadLine().ToUpper() == "Y");
+        }
+
+        private static string GetPlayerName()
+        {
+            Console.Write("Bitter gib Deinen Namen ein: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Willkommen, {0}. Viel Spaß!!\n", name);
+            return name;
         }
 
         private static void Play()
@@ -21,29 +36,37 @@ namespace MasterMind
             bool won = false;
             while (won == false) {
                 int[] userGuess = GetUserGuess(numberCount);
-
                 if (CountHits(PCArray, userGuess) == numberCount)
                     won = true;
             }
             if (won)
                 Console.WriteLine("You win!");
             else
-                Console.WriteLine("Oh no! You couldn't guess the right number.");
-            Console.Write("The correct number is: ");
+                Console.WriteLine("Oh nein! Du hast die Zahl nicht erraten.");
+            Console.Write("Die richtige Zahl lautet: ");
             for (int j = 0; j < numberCount; j++)
                 Console.Write(PCArray[j] + " ");
             Console.WriteLine();
-           // Console.Read();
         }
 
         private static int CountHits(int[] PCArray, int[] userGuess)
         {
+            int metaHits = 0;
+
+            for (int i = 0; i < PCArray.Length; i++) {
+                if (userGuess.Any(item => item == PCArray[i]) && (PCArray[i] != userGuess[i])) {
+                    metaHits++;
+                }
+
+            }
+            Console.WriteLine("Sie haben " + metaHits + " Fast-Treffer, die richtige Zahl, aber an der falschen Position");
+
             int hits = 0;
             for (int i = 0; i < PCArray.Length; i++) {
                 if (PCArray[i] == userGuess[i])
                     hits++;
             }
-             Console.WriteLine("Results: {0} Hit(s), {1} Miss(es)", hits, PCArray.Length - hits);
+            Console.WriteLine("Ergebnis: {0} Treffer, {1} Daneben, {2} Fast-Treffer", hits, PCArray.Length - metaHits - hits, metaHits);
             return hits;
         }
 
@@ -54,11 +77,11 @@ namespace MasterMind
             for (int i = 0; i < userSize; i++) {
                 Console.Write("Digit {0}: ", (i + 1));
                 while (!int.TryParse(Console.ReadLine(), out number) /*|| number < 1 || number > 4*/)
-                    Console.WriteLine("Invalid number!");
+                    Console.WriteLine("Ungültige Zahl!");
                 userGuess[i] = number;
             }
             Console.WriteLine();
-            Console.Write("Your guess: ");
+            Console.Write("Dein Tip: ");
             for (int i = 0; i < userSize; i++) {
                 Console.Write(userGuess[i] + " ");
             }
@@ -81,7 +104,7 @@ namespace MasterMind
             int singleNumber;
             int[] randomNumber = new int[RandomNumberCount];
             Random rnd = new Random();
-            Console.Write("PC Number : ");
+            Console.Write("Die geheime Zahl ist ausgewählt : ");
             for (int i = 0; i < RandomNumberCount; i++) {
                 singleNumber = rnd.Next(1, 9);
                 randomNumber[i] = singleNumber;
